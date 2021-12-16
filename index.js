829be2ceb5fcb7500bb970b9c0d9297a44789f9c
axoletl <38306961+axoletl@users.noreply.github.com>
2021-12-16 14:23:59 +1100

    nvm that sucked

--- index.js
+++ index.js
@@ -4,28 +4,20 @@ const WebSocket = require('ws');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(express);
-const app = express();
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({server})
 
-server.listen(port, function () {
+server.listen(port, function(){
     console.log('server awake at ' + port);
 });
 
-var options = {
-    index: "index.html"
-};
-
-//serve http test page
-app.use('/', express.static('app', options));
-
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
+wss.on('connection', function connection(ws){
+    ws.on('message', function incoming(data){
 
         console.log(new Buffer.from(data).toString());
 
         //for each connected client
-        wss.clients.forEach(function each(client) {
-            if (client != ws && client.readyState == WebSocket.OPEN) {
+        wss.clients.forEach(function each(client){
+            if (client != ws && client.readyState == WebSocket.OPEN){
                 //sends data as unspecified blob
                 client.send(data);
             }
